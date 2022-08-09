6d344efd309ec1e0d7b967d1db957a51643dcaea
Carlos Genuino <carloxsgenuino3@gmail.com>
2022-08-09 18:22:13 -0300

    expondo a rota de create user

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,7 +5,7 @@ import { Specification } from "../modules/cars/entities/Specification";
 
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "db",
     port: 5432,
     username: "develop",
     password: "123456",
