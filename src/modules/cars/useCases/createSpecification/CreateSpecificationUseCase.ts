61a8cb0b9e700fab4f37531a2989245b6ad8cb43
Carlos Alexandre Silva Genuino <carloxsgenuino3@live.com>
2022-08-12 01:38:37 -0300

    validando spec

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,5 +1,6 @@
 
 import { inject, injectable } from "tsyringe";
+import { ApiError } from "../../../../errors/ApiError";
 import { IRequest } from "../../repositories/ICategoryRepository";
 import { ISpecificationRepository } from "../../repositories/ISpecificationRepository";
 
@@ -11,6 +12,10 @@ class CreateSpecificationUseCase {
     }
 
     async execute({name, description}: IRequest){
+        const spec = await this.repo.findByName(name);
+        if(spec){
+            throw new ApiError("specification already exists")
+        }
         this.repo.create({name, description});
     }
 }
